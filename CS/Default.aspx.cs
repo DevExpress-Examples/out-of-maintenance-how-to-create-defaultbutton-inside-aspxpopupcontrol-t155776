baf35310270a4c6566299f6df5c7b0ac500cc23b
DevExpressExampleBot <fakeEmail>
2022-12-07 15:53:49 +0400

    Source auto update [skip ci]

--- CS/Default.aspx.cs
+++ CS/Default.aspx.cs
@@ -5,7 +5,7 @@ public partial class _Default : Page {
     protected void Button1_Click(object sender, EventArgs e) {
         Label.Text = "Button1 is clicked";
     }
-    protected void Popup_WindowCallback(object source, DevExpress.Web.ASPxPopupControl.PopupWindowCallbackArgs e) {
+    protected void Popup_WindowCallback(object source, DevExpress.Web.PopupWindowCallbackArgs e) {
         Label.Text = "Button2 is clicked";
     }
 }
--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -7,7 +7,7 @@ Partial Public Class _Default
 	Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs)
 		Label.Text = "Button1 is clicked"
 	End Sub
-	Protected Sub Popup_WindowCallback(ByVal source As Object, ByVal e As DevExpress.Web.ASPxPopupControl.PopupWindowCallbackArgs)
+	Protected Sub Popup_WindowCallback(ByVal source As Object, ByVal e As DevExpress.Web.PopupWindowCallbackArgs)
 		Label.Text = "Button2 is clicked"
 	End Sub
 End Class
